d1f81687af51c75000fb3f0286ecc6206c473375
Arcon <632112808@qq.com>
2017-11-21 21:06:52 +0800

    Merge branch 'master' of https://github.com/ArconB/LAB_seq-malGAN
    # Conflicts:
    #	tensorflow_code/main.py
    #	tensorflow_code/networks.py

--- tensorflow_code/malGAN.py
+++ tensorflow_code/malGAN.py
@@ -16,9 +16,9 @@ class malGAN():
 
     def __init__(self, G_cell_type='LSTM', G_rnn_layers=[128], G_is_bidirectional=False, G_attention_layers=None,
                  G_ff_layers=[128], D_cell_type='LSTM', D_rnn_layers=[128], D_is_bidirectional=False,
-                 D_attention_layers=[128], D_ff_layers=[128], batch_size=128, num_token=161, max_seq_len=1024,
-                 num_class=2, learning_rate=0.001, scope='malGAN', model_path='./black_box_d_model',
-                 log_path='./log.txt'):
+                 D_attention_layers=[128], D_ff_layers=[128], G_max_seq_len=1024, D_max_seq_len=2048,
+                 batch_size=128, num_token=161, num_class=2, learning_rate=0.001, scope='malGAN',
+                 model_path='./black_box_d_model', log_path='./log.txt'):
         """
         initialize malGAN model
         :param G_cell_type:
@@ -44,7 +44,6 @@ class malGAN():
         # define self holding variables
         self.batch_size = batch_size
         self.num_token = num_token
-        self.max_seq_len = max_seq_len
         self.num_class = num_class
         self.learning_rate = learning_rate
         self.model_path = model_path
@@ -53,4 +52,37 @@ class malGAN():
         # define network structure
         g_malGAN = tf.Graph()
         with g_malGAN.as_default():
-            def generator(scope='')
+            # define network structure function
+            def generator(input, input_len, z, scope='malGAN_G', reuse=None):
+                with tf.variable_scope(scope, reuse=reuse):
+                    output = input
+                    # todo: Generator's structure
+
+                return output
+
+            def discriminator(input, input_len, scopr='malGAN_D', reuse=None):
+                with tf.variable_scope(scope, reuse=reuse):
+                    output = input
+                    # todo: Discriminator's structure
+
+                return output
+
+            # define placeholder for input, input_len and target
+            self.input = tf.placeholder(tf.int32, [None, G_max_seq_len])
+            self.input_len = tf.placeholder(tf.int32, [None])
+            # todo
+
+            # get output from network structure
+            # todo
+
+            # calculate loss
+            # todo
+
+            # define optimizer, compute gradients and apply gradients
+            # todo
+
+            # define init_opt, saver
+
+        # define sess
+        # todo
+
